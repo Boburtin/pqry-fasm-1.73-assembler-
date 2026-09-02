3f2552e09b7f2f928e4a8ae28650f871cf678a27
boburtin <rs.aubertin@gmail.com>
2026-09-02 07:44:11 -0700

    add test cases

--- src/TokUtils.h
+++ src/TokUtils.h
@@ -1,19 +1,79 @@
 #ifndef TOK_UTILS_H
 #define TOK_UTILS_H
 
-inline bool isEOF(char c) { return c == '\0' || c == 0x1A; }
-inline bool isSpace(char c) { return c == ' ' || c == '\t'; }
-inline bool isEOL(char c) { return c == '\r' || c == '\n'; }
-inline bool isSymbol(char c) {
-    return (c >= 0x30 && c <= 0x39) || (c >= 65 && c <= 90) ||
-           (c >= 97 && c <= 122) || c == '_' || c == '.' || c == '$' ||
-           c == '!' || c == '%' || c == '@' || c == '?';
-}
-inline bool isPunct(char c) {
-    return c == '+' || c == '-' || c == '/' || c == '*' || c == '=' ||
-           c == '<' || c == '>' || c == '(' || c == ')' || c == '[' ||
-           c == ']' || c == '{' || c == '}' || c == ':' || c == ',' ||
-           c == '|' || c == '&' || c == '~' || c == '#' || c == '`';
-}
+#include "Prim.h"
+#include "Source.h"
+
+class TMaker {
+   public:
+    static bool isEOF(char c) { return c == '\0' || c == 0x1A; }
+    static bool isSpace(char c) { return c == ' ' || c == '\t'; }
+    static bool isEOL(char c) { return c == '\r' || c == '\n'; }
+    static bool isSymbol(char c) {
+        return (c >= 0x30 && c <= 0x39) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122) ||
+               c == '_' || c == '.' || c == '$' || c == '!' || c == '%' || c == '@' || c == '?';
+    }
+    static bool isPunct(char c) {
+        return c == '+' || c == '-' || c == '/' || c == '*' || c == '=' || c == '<' || c == '>' ||
+               c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}' || c == ':' ||
+               c == ',' || c == '|' || c == '&' || c == '~' || c == '#' || c == '`';
+    }
+    struct Tkn {
+        TokKind kind;
+        u32 start, end;
+    };
+
+    TMaker(const Source& src) : src_(src) {}
+
+    char get() const { return src_.at(idx_); }
+
+    void scan(TokArray& out) {
+        for (;;) {
+            auto [k, s, e] = next();
+            out.push(k, s, e);
+            if (k == TokKind::Eof) break;
+        }
+    }
+
+   private:
+    Tkn next() {
+        /* skip tabs & spaces */
+        while (isSpace(get())) idx_++;
+
+        /* get non-space char and pin start */
+        char c{get()};
+        u32 start{idx_};
+
+        switch (c) {
+            case 0x1A:  // do not advance idx_
+            case 0x00:
+                return {TokKind::Eof, idx_, idx_};
+            case 0x27:  // quotes
+            case 0x22: {
+                idx_++;  // point to first char
+                char d{get()};
+                while (!isEOF(d) && c != d) {
+                    idx_++;
+                    d = get();  // adv and fetch until d = <', ", EOF>
+                }
+                return {TokKind::String, start, (d == c ? ++idx_ : idx_)};
+            }
+            case 0x0A:  // LF/CR
+            case 0x0D: {
+                idx_ += (src_.at(idx_ + 1) ^ c == 0x07 ? 2 : 1);
+                return {TokKind::Endl, start, idx_};
+            }
+            default:
+                if (isPunct(c)) return {TokKind::Punct, start, ++idx_};
+                while (!isEOF(c) && !isSpace(c) && !isEOL(c)) {
+                    idx_++;
+                    c = get();
+                }
+                return {TokKind::Symbol, start, idx_};
+        }
+    }
+    const Source& src_;
+    u32 idx_ {};
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 
 #include "Prim.h"
 #include "Source.h"
-#include "Tok.h"
+#include "TokUtils.h"
+
+const char* Symbol = "\033[3;34mSymbol\033[0m";
+const char* Endl = "\033[1;33mEndl\033[0m";
+const char* Eof = "\033[1;32mEOF!\033[0m";
+const char* Punct = "\033[3;31mPunct\033[0m";
+const char* String = "\033[3;30mString\033[0m";
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -10,25 +16,25 @@ int main(int argc, char** argv) {
     }
     auto src = Source::fromFile(argv[1]);
     auto toks = TokArray(src.size() + 1);
-    auto tmaker = TokMaker(src);
+    auto tmaker = TMaker(src);
     tmaker.scan(toks);
 
     for (u64 i{}; i < toks.size; ++i) {
         switch (toks.kinds[i]) {
             case TokKind::Symbol:
-                printf("[%llu] = Symbol\n", i);
+                printf("\n[%llu] = %s", i, Symbol);
                 break;
             case TokKind::Endl:
-                printf("[%llu] = Endl\n", i);
+                printf(", [%llu] %s", i, Endl);
                 break;
             case TokKind::Eof:
-                printf("[%llu] = Eof\n", i);
+                printf("\n[%llu] = %s", i, Eof);
                 break;
             case TokKind::Punct:
-                printf("[%llu] = Punct\n", i);
+                printf("\n[%llu] = %s", i, Punct);
                 break;
             case TokKind::String:
-                printf("[%llu] = String\n", i);
+                printf("\n[%llu] = %s", i, String);
                 break;
         }
     }
